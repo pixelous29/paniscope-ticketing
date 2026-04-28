3c7a24da8dabfe1ee2d4418133a2d0abe7403606
Yves Le Signor <ylesignor@gmail.com>
2026-04-28 16:39:59 +0200

    fix: update CI Node.js version and ignore android folder in ESLint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -5,7 +5,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', 'dev-dist']),
+  globalIgnores(['dist', 'dev-dist', 'android', 'ios']),
   {
     files: ['**/*.{js,jsx}'],
     extends: [
